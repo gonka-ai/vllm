d7fdfa109cf7bb8f4d578c81aa82e67a3f43c18d
Gleb Morgachev <morgachev.g@gmail.com>
2026-02-22 02:15:59 +0000

    Changes from fi/fix-request-abort

--- vllm/engine/multiprocessing/engine.py
+++ vllm/engine/multiprocessing/engine.py
@@ -448,7 +448,8 @@ class MQLLMEngine:
         for scheduler in self.engine.scheduler:
             for queue in [scheduler.waiting, scheduler.running, scheduler.swapped]:
                 for seq_group in list(queue):
-                    self.engine.abort_request(seq_group.request_id)
+                    group = self.engine.seq_id_to_seq_group.get(seq_group.request_id)
+                    self.engine.abort_request(group.group_id if group is not None else seq_group.request_id)
                     aborted += 1
         return aborted
 
